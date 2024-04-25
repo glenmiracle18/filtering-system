6c9f69ed2beb41c1bad1cd7699ed33d7540ba7da
glenmiracle18 <m.bonyu@alustudent.com>
2024-04-25 13:42:52 +0200

    final: final touches and bug handling

--- app/page.tsx
+++ app/page.tsx
@@ -83,8 +83,6 @@ export default function Home() {
     size: ["S", "L", "M"],
   });
 
-  console.log(filter);
-
   // quick data fetch demo using tanstack query
   const { data: products, refetch } = useQuery({
     queryKey: ["products"],
@@ -142,13 +140,16 @@ export default function Home() {
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-          High-quality cotton selection
+        <h1 className="text-4xl font-bold tracking-tight text-green-900">
+          High-quality cotton selection{" "}
+          <p className="tracking tight font-medium text-sm mt-2 text-green-600">
+            ( by Glen Miracle)
+          </p>
         </h1>
 
         <div className="flex items-center">
           <DropdownMenu>
-            <DropdownMenuTrigger className="group inline-flex text-sm font-medium text-gray-700 hover:text-gray-900">
+            <DropdownMenuTrigger className="group inline-flex text-sm font-medium text-green-700 hover:text-green-900">
               Sort
               <ChevronDown className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
             </DropdownMenuTrigger>
